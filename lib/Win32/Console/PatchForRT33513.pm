d2dc4cbbf75a5f68e9d20782102c72bedb52c7cd
kuerbis <cuer2s@gmail.com>
2022-05-09 09:14:33 +0200

    Removed unnecessary files

--- lib/Win32/Console/PatchForRT33513.pm
+++ lib/Win32/Console/PatchForRT33513.pm
@@ -3,7 +3,7 @@ package Win32::Console::PatchForRT33513;
 use strict;
 use warnings;
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 use Win32::Console qw( );
 
@@ -51,7 +51,7 @@ Win32::Console::PatchForRT33513 - Patch for RT33513.
 
 =head1 VERSION
 
-Version 0.003
+Version 0.004
 
 =cut
 
